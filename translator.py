a9036a37852966c21e48831f4cbbd03480456397
manojnathyogi <85493350+manojnathyogi@users.noreply.github.com>
2021-09-27 23:09:06 +0545

    api and the url are hidden

--- translator.py
+++ translator.py
@@ -3,8 +3,8 @@ import json
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from pandas import json_normalize
 
-url_lt = "https://api.us-south.language-translator.watson.cloud.ibm.com/instances/971ca9f6-ea42-4f03-86bd-f290c32de433"
-apikey_lt = "_wETvMMpcZVt0YObpse2_jToi-pE07zyfP0CBDsfkAQe"
+url_lt = "https://api.us-south.language-translator.watson.cloud.ibm.com/instances/---Hidden---"
+apikey_lt = "Hidden For Security"
 version_lt ="2018-05-01"
 authenticator = IAMAuthenticator(apikey_lt)
 language_translator = LanguageTranslatorV3(version=version_lt, authenticator=authenticator)
